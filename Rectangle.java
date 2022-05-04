10d5327d37c22028435d14bfeaf6af1c9cd62a1f
huudao805 <104880369+huudao805@users.noreply.github.com>
2022-05-04 10:19:14 +0700

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle implements IShape {
+	private int width;
+  private int height;
+  
+  public Rectangle(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
+  
+	public double acreageShape() {
+		return width*height;
+	}
+}
